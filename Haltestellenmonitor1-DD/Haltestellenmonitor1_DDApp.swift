7f2904f69cd7bfe8237f20b2882e86123dd7df73
Peter Lohse <hanashi@hanashi.eu>
2023-04-18 13:30:18 +0200

    Initial Commit

---
+++ Haltestellenmonitor1-DD/Haltestellenmonitor1_DDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haltestellenmonitor1_DDApp.swift
+//  Haltestellenmonitor1-DD
+//
+//  Created by Peter Lohse on 18.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Haltestellenmonitor1_DDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
