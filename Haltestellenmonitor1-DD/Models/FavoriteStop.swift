a4279c0e2a49249d29f26ce4f90944cfce25e44c
Tom Braune <tom_braune@icloud.com>
2023-11-06 21:01:32 +0100

    fixed last group (oversaw this one... :/ )

--- Haltestellenmonitor1-DD/Models/FavoriteStop.swift
+++ Haltestellenmonitor1-DD/Models/FavoriteStop.swift
@@ -11,7 +11,7 @@ import Foundation
     @Published var favorites: [Int]
     
     init() {
-        if let data = UserDefaults(suiteName: "group.dev.hanashi.Haltestellenmonitor")?.data(forKey: "FavoriteStops") {
+        if let data = UserDefaults(suiteName: "group.eu.hanashi.Haltestellenmonitor")?.data(forKey: "FavoriteStops") {
             if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
                 favorites = decoded
                 return
