adc365c9f4e01e0cdcde5ad953c2f1852c3d1a90
Tom <65157178+t0mbrn@users.noreply.github.com>
2025-04-20 11:16:28 +0200

    Improved Styling of Warnungen button

--- Haltestellenmonitor1-DD/Views/SingleTripView.swift
+++ Haltestellenmonitor1-DD/Views/SingleTripView.swift
@@ -28,8 +28,12 @@ struct SingleTripView: View {
                         NavigationLink {
                             DepartureInfoView(stopEvent: stopEvent)
                         } label: {
-                            Text("Meldungen")
+                            Image(systemName: "exclamationmark.triangle.fill")
+                            Text("Aktuelle Meldungen")
+
                         }
+                        .buttonStyle(BorderedProminentButtonStyle())
+                        .tint(Color.orange)
                     }
                     
                     List(searchResults, id: \.self) { stopSequenceItem in
@@ -184,23 +188,23 @@ struct SingleTripView: View {
     }
 }
 
-//struct SingleTripView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        NavigationStack {
-//            SingleTripView(stopSequence: [StopSequenceItem(id: "1", name: "HBF", type: "", niveau: 1, productClasses: [1, 4], properties: StopSequenceItem.properties(AREA_NIVEAU_DIVA: "", DestinationText: "Schwimmbad", area: "", platform: "1"))], stop: Stop.getByGID(gid: "de:14612:28")!,
-//                           stopEvent: StopEvent(
-//                            location: Location(Id: "de:14612:28", IsGlobalId: true, Name: "HBF DD", DisassembledName: "", type: "stop", Coord: [], Properties: Stop_Property(stopId: "de:14612:28")),
-//                            departureTimePlanned: "2025-03-26T06:00:00Z",
-//                            departureTimeBaseTimetable: "2025-03-26T06:00:00Z",
-//                            transportation: Transportation(id: "ddb:98X27: :R:j25", name: "ICE 870 InterCityExpress", number: "870",
-//                                                           product: Product(id: 0, class: 0, name: "Zug", iconId: 6),
-//                                                           properties: T_Properties(),
-//                                                           destination: Place(id: "", name: "", type: "")),
-//                            infos: [Info(priority: "Medium", id: "", version: 1, type: "Linienänderung",
-//                                         infoLinks: [ InfoLink(urlText: "", url: "", content: "Hallo Welt", subtitle: "hi")]),
-//                                    Info(priority: "Medium", id: "", version: 1, type: "Linienänderung2",
-//                                         infoLinks: [ InfoLink(urlText: "", url: "", content: "Hallo Welt wie geht es dir heute mir geht es gut und dir auch? das hier ist jetzt ganz viel Text um die expansion weiter zu testen, wenn der Text länger ist soll nämlich die View weiter nach unten expandiert werden", subtitle: "hi", title: "Test"), InfoLink(urlText: "", url: "", content: "<h1>Hallö Welt</h1><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>", subtitle: "hi", title: "Test2")])]))
-//            
-//        }.environmentObject(PushTokenHistory())
-//    }
-//}
+struct SingleTripView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            SingleTripView(stopSequence: [StopSequenceItem(id: "1", name: "HBF", type: "", niveau: 1, productClasses: [1, 4], properties: StopSequenceItem.properties(AREA_NIVEAU_DIVA: "", DestinationText: "Schwimmbad", area: "", platform: "1"))], stop: Stop.getByGID(gid: "de:14612:28")!,
+                           stopEvent: StopEvent(
+                            location: Location(Id: "de:14612:28", IsGlobalId: true, Name: "HBF DD", DisassembledName: "", type: "stop", Coord: [], Properties: Stop_Property(stopId: "de:14612:28")),
+                            departureTimePlanned: "2025-03-26T06:00:00Z",
+                            departureTimeBaseTimetable: "2025-03-26T06:00:00Z",
+                            transportation: Transportation(id: "ddb:98X27: :R:j25", name: "ICE 870 InterCityExpress", number: "870",
+                                                           product: Product(id: 0, class: 0, name: "Zug", iconId: 6),
+                                                           properties: T_Properties(),
+                                                           destination: Place(id: "", name: "", type: "")),
+                            infos: [Info(priority: "Medium", id: "", version: 1, type: "Linienänderung",
+                                         infoLinks: [ InfoLink(urlText: "", url: "", content: "Hallo Welt", subtitle: "hi")]),
+                                    Info(priority: "Medium", id: "", version: 1, type: "Linienänderung2",
+                                         infoLinks: [ InfoLink(urlText: "", url: "", content: "Hallo Welt wie geht es dir heute mir geht es gut und dir auch? das hier ist jetzt ganz viel Text um die expansion weiter zu testen, wenn der Text länger ist soll nämlich die View weiter nach unten expandiert werden", subtitle: "hi", title: "Test"), InfoLink(urlText: "", url: "", content: "<h1>Hallö Welt</h1><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>", subtitle: "hi", title: "Test2")])]))
+            
+        }.environmentObject(PushTokenHistory())
+    }
+}
