1143d5527bd4654d79ccfb24f73d1f4d8dfefe81
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2025-05-12 09:47:26 +0200

    WIP update Map to ios17

--- Haltestellenmonitor1-DD/Views/MapView.swift
+++ Haltestellenmonitor1-DD/Views/MapView.swift
@@ -8,30 +8,52 @@
 import SwiftUI
 import MapKit
 
+
 struct MapView: View {
     @EnvironmentObject var locationManager: LocationManager
     @EnvironmentObject var stopManager: StopManager
     @State var tracking: MapUserTrackingMode = .none
-
+    
+    @State private var pos = (MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.050446, longitude: 13.737954),
+        span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+    ))
+    
     var body: some View {
         NavigationStack(path: $stopManager.presentedMapStops) {
-            Map(coordinateRegion: locationManager.region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $tracking, annotationItems: stops, annotationContent: { stop in
-                MapAnnotation(coordinate: stop.coordinates, content: {
-                    NavigationLink(value: stop) {
-                        Image(systemName: "h.circle.fill")
-                            .foregroundColor(Color("MapColor"))
-                            .background(Circle().fill(Color(.systemBackground)) .shadow(radius: 1))
+            
+            if #available(iOS 17.0, *) {
+                Map(initialPosition: .region(pos)) {
+                    ForEach(stops) { stop in
+                        Annotation(stop.name, coordinate: stop.coordinates) {
+                            NavigationLink(value: stop) {
+                                Image(systemName: "h.circle.fill")
+                                    .foregroundColor(Color("MapColor"))
+                                    .background(Circle().fill(Color(.systemBackground)) .shadow(radius: 1))
+                            }
+                        }
                     }
+                }
+            } else {
+                Map(coordinateRegion: locationManager.region, interactionModes: .all, showsUserLocation: true, userTrackingMode: $tracking, annotationItems: stops, annotationContent: { stop in
+                    MapAnnotation(coordinate: stop.coordinates, content: {
+                        NavigationLink(value: stop) {
+                            Image(systemName: "h.circle.fill")
+                                .foregroundColor(Color("MapColor"))
+                                .background(Circle().fill(Color(.systemBackground)) .shadow(radius: 1))
+                        }
+                    })
                 })
-            })
-            .ignoresSafeArea(edges: .top)
-            .navigationDestination(for: Stop.self) { stop in
-                DepartureView(stop: stop)
+                .ignoresSafeArea(edges: .top)
+                .navigationDestination(for: Stop.self) { stop in
+                    DepartureView(stop: stop)
+                }
             }
         }
     }
 }
 
+@available(iOS 17.0, *)
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
