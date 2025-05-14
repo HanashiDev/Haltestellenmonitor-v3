906862bcc3141b12796b2c9b561637150a6730b2
Tom <65157178+t0mbrn@users.noreply.github.com>
2025-05-14 17:05:27 +0200

    Add accessibility Labels to About

--- Haltestellenmonitor1-DD/Views/About.swift
+++ Haltestellenmonitor1-DD/Views/About.swift
@@ -16,12 +16,13 @@ struct About: View {
                         Text("Version")
                         Spacer()
                         Text("\(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unbekannt") (\(Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unbekannt"))")
-                    }
+                    }.accessibilityElement(children: .combine)
                 }
 
                 Section(header: Text("Kontakt")) {
                     Link(destination: URL(string: "mailto:info@hanashi.dev?subject=Haltestellenmonitor%20Dresden%20Feedback")!) {
                         Text(verbatim: "📧 info@hanashi.dev")
+                            .accessibilityHint("Email an den App-Entwickler schicken")
                     }
                     Link(destination: URL(string: "https://hanashi.dev")!) {
                         Text(verbatim: "🌍 Homepage")
@@ -37,6 +38,8 @@ struct About: View {
                                 .offset(x: -1)
 
                         }
+                        .accessibilityElement(children: .combine)
+                        .accessibilityHint("GitHub Repository mit Quellcode zur App aufrufen")
                     }
                 }
             }
